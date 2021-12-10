e2036eba830cf7129e5e558ad70610bf6d5d18b8
CMooney-01 <chris.mooney@blackflagbarbell.com.au>
2021-12-10 14:23:11 +1100

    Added create or insert functionality to insert form data after user clicks submit button on sign up page

--- server.js
+++ server.js
@@ -65,16 +65,16 @@ app.get('/payment', function (req, res) {
     // If not, need to create one
     // If exists, need to add user data to datbase table
 
-    connection.query(`
-      SELECT COUNT(*)
-      FROM information_schema.tables
-      WHERE table_schema = "Upcoming_Events"
-      AND table_name = "Entries1"
-      `, function(err, result) {
-      if (err) throw err;
-      console.log(result);
-      res.send(result);
-    });
+    // connection.query(`
+    //   SELECT COUNT(*)
+    //   FROM information_schema.tables
+    //   WHERE table_schema = "Upcoming_Events"
+    //   AND table_name = "Entries1"
+    //   `, function(err, result) {
+    //   if (err) throw err;
+    //   console.log(result);
+    //   res.send(result);
+    // });
 
     connection.end();
   })
@@ -82,6 +82,9 @@ app.get('/payment', function (req, res) {
 
 app.post('/event-signup', function(req, res) {
 
+  // console.log(req.body);
+
+  let compID = req.body.confirm
   let lifterName = req.body.lifterName;
   let lifterEmail = req.body.lifterEmail;
   let lifterPhone = req.body.lifterPhone;
@@ -103,13 +106,35 @@ app.post('/event-signup', function(req, res) {
         if (err) throw err;
       });
 
-      var sql =`INSERT INTO Entries1 (entry_id, comp_id, lifter_name, lifter_email, lifter_phone, lifter_dob) VALUES
-      ("01", "01", "${lifterName}", "${lifterEmail}", "${lifterPhone}", "${lifterDob}")`
 
-      connection.query(sql, function(err, result) {
-        if (err) throw err;
-        console.log('Lifter info submitted successfully');
-      });
+        var sql = `
+          CREATE TABLE IF NOT EXISTS Entries${compID} (
+            entry_id INT NOT NULL AUTO_INCREMENT,
+            comp_id INT NOT NULL,
+            lifter_name VARCHAR(255) NOT NULL,
+            lifter_email VARCHAR(255) NOT NULL,
+            lifter_phone VARCHAR(20) NOT NULL,
+            lifter_dob DATE NOT NULL,
+            PRIMARY KEY (entry_id),
+            FOREIGN KEY (comp_id) REFERENCES Events(event_id)
+          );
+          `
+
+          connection.query(sql, function(err, result) {
+            if (err) throw err;
+            console.log("Table created");
+          });
+
+          var sql = `
+            INSERT INTO Entries${compID}
+            (comp_id, lifter_name, lifter_email, lifter_phone, lifter_dob)
+            VALUES
+            ('${compID}', '${lifterName}', '${lifterEmail}', '${lifterPhone}', '${lifterDob}');
+          `
+          connection.query(sql, function(err, result) {
+            if (err) throw err;
+            console.log("Lifter info inserted");
+          });
 
       connection.end();
     })
