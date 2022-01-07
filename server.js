c992a20490b155a859d4848e7fb740ec2fc8167f
CMooney-01 <chris.mooney@blackflagbarbell.com.au>
2022-01-07 16:52:54 +1100

    Updated to make competition date more readable

--- server.js
+++ server.js
@@ -3,9 +3,28 @@ const express = require('express');
 const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
+app.use(express.static("public"));
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const mysql = require('mysql');
+const stripe = require('stripe')('sk_test_51K50drLJQcpU2qlqFdNM52tcYNHyP21Ee2Pj991UKzzJjgS4M75eeQBGWKnowaG6zQpgYgI1zN40fhYJHQnQIYKz00RTIQU0ev');
+
+// Stripe setup
+
+app.post("/create-payment-intent", async (req, res) => {
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 6500,
+    currency: 'aud',
+    payment_method_types: ['card'],
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+// End Stripe setup
 
 const connection = mysql.createConnection({
   host: process.env.RDS_HOSTNAME,
@@ -36,7 +55,8 @@ app.get('/events', function (req, res) {
       if (err) throw err;
     });
 
-    var sql = "SELECT * FROM Events"
+    var sql = "SELECT *, DATE_FORMAT(event_date, '%a the %D of %b, %Y') as NewDateField FROM Events"
+
     connection.query(sql, function(err, result) {
       if (err) throw err;
       console.log(result);
@@ -48,43 +68,43 @@ app.get('/events', function (req, res) {
 
 });
 
-app.get('/payment', function (req, res) {
-
-  const connection = mysql.createConnection({
-    host: process.env.RDS_HOSTNAME,
-    user: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
-    port: process.env.RDS_PORT
-  });
-
-  connection.connect(function(err) {
-    if (err) throw err;
-    console.log('Payments route');
-
-    // Need to check if database for comp entries currently exists
-    // If not, need to create one
-    // If exists, need to add user data to datbase table
-
-    // connection.query(`
-    //   SELECT COUNT(*)
-    //   FROM information_schema.tables
-    //   WHERE table_schema = "Upcoming_Events"
-    //   AND table_name = "Entries1"
-    //   `, function(err, result) {
-    //   if (err) throw err;
-    //   console.log(result);
-    //   res.send(result);
-    // });
-
-    connection.end();
-  })
-});
+// app.get('/payment', function (req, res) {
+//
+//   const connection = mysql.createConnection({
+//     host: process.env.RDS_HOSTNAME,
+//     user: process.env.RDS_USERNAME,
+//     password: process.env.RDS_PASSWORD,
+//     port: process.env.RDS_PORT
+//   });
+//
+//   connection.connect(function(err) {
+//     if (err) throw err;
+//     console.log('Payments route');
+//
+//     // Need to check if database for comp entries currently exists
+//     // If not, need to create one
+//     // If exists, need to add user data to datbase table
+//
+//     // connection.query(`
+//     //   SELECT COUNT(*)
+//     //   FROM information_schema.tables
+//     //   WHERE table_schema = "Upcoming_Events"
+//     //   AND table_name = "Entries1"
+//     //   `, function(err, result) {
+//     //   if (err) throw err;
+//     //   console.log(result);
+//     //   res.send(result);
+//     // });
+//
+//     connection.end();
+//   })
+// });
 
 app.post('/event-signup', function(req, res) {
 
-  // console.log(req.body);
+  console.log(req.body.comp);
 
-  let compID = req.body.confirm
+  let compID = parseInt(req.body.comp, 10)
   let lifterName = req.body.lifterName;
   let lifterEmail = req.body.lifterEmail;
   let lifterPhone = req.body.lifterPhone;
@@ -139,5 +159,5 @@ app.post('/event-signup', function(req, res) {
       connection.end();
     })
 
-    res.redirect("/payment");
+    res.redirect("/payment-success");
 });
